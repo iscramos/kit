f5a62a0db93f51d0113cce45be42cfb9b6de54cf
Leodan Ramos Giron <lramos@naturesweet.com>
2019-08-08 09:05:36 -0500

    Se agrega para visualizar datos mayores al 2018

--- indexMedicionesRebombeo.php
+++ indexMedicionesRebombeo.php
@@ -10,11 +10,12 @@ require_once('includes/inc.session.php');
 
 if($_SESSION["type"] == 1 || $_SESSION["type"] == 7) // para admin y hidroelectrico
 {
-	$consulta = "SELECT bd_rebombeo.*,  ( if(bd_rebombeo.m_consumidos > 0, bd_rebombeo.m_consumidos * 10, '') ) AS m_multiplicado, (DATE_FORMAT(bd_rebombeo.fechaLectura, '%d-%m-%Y %H:%i')) AS fecha_fomateada, disponibilidad_activos.descripcion, tipoMedicion_rebombeo.descripcion as tipoM
+	$consulta = "SELECT bd_rebombeo.*,  ( if(bd_rebombeo.m_consumidos > 0, bd_rebombeo.m_consumidos * 10, '') ) AS m_multiplicado, (DATE_FORMAT(bd_rebombeo.fechaLectura, '%d/%m/%Y %H:%i')) AS fecha_fomateada, disponibilidad_activos.descripcion, tipoMedicion_rebombeo.descripcion as tipoM
 					FROM bd_rebombeo
 					INNER JOIN disponibilidad_activos ON bd_rebombeo.equipo = disponibilidad_activos.activo
 					INNER JOIN tipoMedicion_rebombeo ON bd_rebombeo.tipo = tipoMedicion_rebombeo.id
 					WHERE disponibilidad_activos.organizacion = 'COL'
+					AND DATE_FORMAT(bd_rebombeo.fechaLectura, '%Y-%m-%d') >= '2018-12-30'
 					ORDER BY bd_rebombeo.fechaLectura DESC, disponibilidad_activos.descripcion ASC, tipoMedicion_rebombeo.descripcion ASC";
 	$mediciones = Bd_rebombeo::getAllByQuery($consulta);
 	// Include page view
