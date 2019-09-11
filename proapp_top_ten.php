5c0d86bab54cc99f55c3fbc24996518e6c886742
Leodan Ramos Giron <lramos@naturesweet.com>
2019-09-11 17:34:53 -0500

    Se agregan más correos para el top ten de equipos criticos (proapp)

--- proapp_top_ten.php
+++ proapp_top_ten.php
@@ -3,7 +3,7 @@
     
     $familia_critica = array("ELECTRICA", "EMBARQUES", "FUMIGACION", "METROLOGIA", "REMOLQUES", "RIEGO", "TRACTORES");
 
-    $arrays = array("orfanelr@naturesweet.com", "sbarajas@naturesweet.com", "lramos@naturesweet.com");
+    $arrays = array("lramos@naturesweet.com", "tecnicocol@naturesweet.com", "sbarajas@naturesweet.com", "mmatias@naturesweet.com", "kmontano@naturesweet.com", "gangulo@naturesweet.com", "orfanelr@naturesweet.com", "lguzman@naturesweet.com", "javalos@naturesweet.com", "fhernandez@naturesweet.com", "yperalta@naturesweet.com", "emontero@naturesweet.com", "mcontreras@naturesweet.com", "efrainc@naturesweet.com", "srios@naturesweet.com", "nrodriguez@naturesweet.com", "hernandezj@naturesweet.com", "gernesto@naturesweet.com", "aochoa@naturesweet.com");
 
     function get_nombre_dia($fecha)
     {
